fad1d36141003513a2e138a1c0a9fe4494a42d14
Pablo <pskyvader@hotmail.com>
2019-10-22 17:03:56 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Image(props) {
     var image=props.image;
+    console.log(image);
     return (
         <img className="" src={image} alt={props.title} />
     );
