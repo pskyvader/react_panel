e032afeed2d11aab77cf9b0872d523fdf89c78b5
Pablo <pskyvader@hotmail.com>
2019-10-22 15:36:22 -0500

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -1,20 +1,21 @@
-def init(self,method,params):
-    if len(params)>1:
-        options=set(params[1:])
-    else:
-        options=()
-    if method=='GET':
-        if len(params)>0:
-            return self.get(params[0],options)
+class base:
+    def init(self,method,params):
+        if len(params)>1:
+            options=set(params[1:])
         else:
-            return self.get()
-    elif len(params)>0:
-        if method=='POST':
-            return self.post(params[0],options)
-        elif method=='PUT':
-            return self.put(params[0],options)
-        elif method=='DELETE':
-            return self.delete(params[0],options)
-    else:
-        return {'error':404,'method':method,'params':params}
+            options=()
+        if method=='GET':
+            if len(params)>0:
+                return self.get(params[0],options)
+            else:
+                return self.get()
+        elif len(params)>0:
+            if method=='POST':
+                return self.post(params[0],options)
+            elif method=='PUT':
+                return self.put(params[0],options)
+            elif method=='DELETE':
+                return self.delete(params[0],options)
+        else:
+            return {'error':404,'method':method,'params':params}
                 
--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -1,17 +1,21 @@
-from .base import init
+from .base import base
 
+def init(method,params):
+    l=logo(method,params)
+    return l.init()
 
-def get(id=0,**params):
-    return {'body':{}}
+class logo(base):
+    def get(self,id=0,**params):
+        return {'body':{}}
 
-def post(id,**params):
-    return {'body':{}}
-    
-    
-def put(id,**params):
-    return {'body':{}}
-    
-def delete(id,**params):
-    return {'body':{}}
+    def post(self,id,**params):
+        return {'body':{}}
+        
+        
+    def put(self,id,**params):
+        return {'body':{}}
+        
+    def delete(self,id,**params):
+        return {'body':{}}
 
 
