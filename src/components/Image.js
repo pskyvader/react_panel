88fe40f169c6981409e65f23d4b6b48b0d605297
Pablo <pskyvader@hotmail.com>
2019-10-22 17:14:56 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ const static_folder='public/images'
 function Image(props) {
     var final_image=props.image;
     console.log(final_image,final_image[0]);
-    var static_image=static_folder+final_image['folder']+final_image.parent+final_image.subfolder+final_image.url;
+    var static_image=static_folder+final_image[0]['folder']+final_image.parent+final_image.subfolder+final_image.url;
     return (
         <img className="" src={static_image} alt={props.title} />
     );
