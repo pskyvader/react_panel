efa06f14a3423149e2f8874c53657b033f704960
Pablo <pskyvader@hotmail.com>
2019-10-22 15:51:53 -0500

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 class Logo extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
-  }
+    resource = 'logo'
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: null,
+        };
+    }
+    componentDidMount() {
+        fetch(API + this.resource)
+            .then(response => response.json())
+            .then(data => this.setState({ img: data.img }));
+    }
+    render() {
+        const { img } = this.state;
+        return (
+            <img src="{img}" alt="" />
+        )
+    }
 }
 export default Logo;
