4e12d1bb561961b6e68a81fbfb8016485b74828e
Pablo <pskyvader@hotmail.com>
2019-10-22 13:49:04 -0500

    --- Auto-Git Commit ---

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -1,8 +1,9 @@
 import sys
 import os
 from core.app import app
-import pprint
 from beaker.middleware import SessionMiddleware
+import json
+from gzip import compress
 
 
 sys.path.insert(0, os.path.dirname(__file__))
@@ -15,13 +16,13 @@ def application2(environ, start_response):
     app_web = app(os.path.dirname(__file__))
     main_data = app_web.init(environ)
     ret = main_data["response_body"]
-    
+
+    if not isinstance(ret,str):
+        ret=json.dumps(ret)
+
     if isinstance(ret, str):
         if ret != "":
-            ret = bytes(ret, "utf-8")
-            from gzip import compress
-
-            ret = compress(ret)
+            ret = compress(bytes(ret, "utf-8"))
             main_data["headers"].append(("Accept-encoding", "gzip,deflate"))
             main_data["headers"].append(("Content-Encoding", "gzip"))
         else:
@@ -29,28 +30,8 @@ def application2(environ, start_response):
 
 
     start_response(main_data["status"], main_data["headers"])
-    # if main_data['status']=='200 OK':
-    # print(environ['PATH_INFO'],'total', (datetime.now()-init_time).total_seconds()*1000)
+    return [ret]
 
-    if "is_file" in main_data and main_data["is_file"]:
-        f = open(main_data["file"], "rb")
-        if "wsgi.file_wrapper" in environ:
-            return environ["wsgi.file_wrapper"](f, 1024)
-        else:
-            # print('no filewrapper')
-            return file_wrapper(f, 1024)
-    else:
-        return [ret]
-
-
-def file_wrapper(fileobj, block_size=1024):
-    try:
-        data = fileobj.read(block_size)
-        while data:
-            yield data
-            data = fileobj.read(block_size)
-    finally:
-        fileobj.close()
 
 
 class LoggingMiddleware:
