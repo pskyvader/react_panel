bcd715a1a509c6ea5b56be6ffb13b6f821b67390
Pablo <pskyvader@hotmail.com>
2019-10-22 16:20:09 -0500

    --- Auto-Git Commit ---

--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -1,11 +1,13 @@
 from .base import base
+from app.models.logo import logo
 
 def init(method,params):
     l=logo()
-    return l.init(method,params)
+    return l.init(method,params,logo)
 
 class logo(base):
     def get(self,id=0,**params):
+
         return {'body':{'asdf':'sfd'}}
 
     def post(self,id,**params):
