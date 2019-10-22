f57dc4835402571cc279d8a07582ab4bf0ba95fb
Pablo <pskyvader@hotmail.com>
2019-10-22 13:53:34 -0500

    --- Auto-Git Commit ---

--- api/core/app.py
+++ api/core/app.py
@@ -117,6 +117,9 @@ class app:
         if "headers" not in response:
             # response["headers"] = [("Content-Type", "text/html charset=utf-8")]
             response["headers"] = [("Content-Type", "application/json; charset=utf-8")]
+
+        if "error" in response:
+            response['body']=response
             
         data_return["status"] = "200 OK"
 
