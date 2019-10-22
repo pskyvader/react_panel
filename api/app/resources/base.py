7584c05de0ea04354dbcade0e886870dc0c9855e
Pablo <pskyvader@hotmail.com>
2019-10-22 16:38:25 -0500

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -4,9 +4,9 @@ class base:
     def init(cls,method,params,model):
         cls.model=model
         if len(params)>1:
-            options=params[1:]
+            options=set(params[1:])
         else:
-            options=[]
+            options=()
         if method=='GET':
             if len(params)>0:
                 return cls.get(params[0],options)
@@ -23,15 +23,15 @@ class base:
             return {'error':404,'method':method,'params':params}
 
     @classmethod 
-    def get(cls,id=0,**params):
+    def get(cls,id=0,*params):
         return {'body':{}}
     @classmethod
-    def post(cls,id,**params):
+    def post(cls,id,*params):
         return {'body':{}}
         
     @classmethod
-    def put(cls,id,**params):
+    def put(cls,id,*params):
         return {'body':{}}
     @classmethod
-    def delete(cls,id,**params):
+    def delete(cls,id,*params):
         return {'body':{}}
