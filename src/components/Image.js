491628a4feb22829df8c664f36125e7b868f92df
Pablo <pskyvader@hotmail.com>
2019-10-22 17:22:11 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { object } from 'prop-types';
 
 const static_folder='public/images'
 function Image(props) {
-    var final_image=props.image;
-    console.log(final_image[0]);
-    var static_image=static_folder+final_image['folder']+final_image.parent+final_image.subfolder+final_image.url;
+    if (typeof(props.image)!=object){
+        var image=props.image[0];
+        var static_image=static_folder+image['folder']+image.parent+image.subfolder+image.url;
+    }else{
+        var static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
+    }
     return (
         <img className="" src={static_image} alt={props.title} />
     );
