c98edc745a1042b46edf9b6bb6e13c55e98037e2
Pablo <pskyvader@hotmail.com>
2019-10-22 17:23:41 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,11 +3,11 @@ import { object } from 'prop-types';
 
 const static_folder='public/images'
 function Image(props) {
-    if (typeof(props.image)==object){
+    if (typeof(props.image)=='object'){
         var image=props.image[0];
         var static_image=static_folder+image.folder+image.parent+image.subfolder+image.url;
     }else{
-        static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
+        var static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
     }
     return (
         <img className="" src={static_image} alt={props.title} />
