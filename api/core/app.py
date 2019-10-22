f588794c87c9e1723eab5720d6058dea54af3183
Pablo <pskyvader@hotmail.com>
2019-10-22 14:25:51 -0500

    --- Auto-Git Commit ---

--- api/core/app.py
+++ api/core/app.py
@@ -104,7 +104,7 @@ class app:
                 ],
             }
         else:
-            controller = app.controller_dir + url[0]
+            controller = app.resource_dir + url[0]
             my_file = Path(app.root + controller + ".py")
             if my_file.is_file():
                 current_module = importlib.import_module(controller.replace("/", "."))
