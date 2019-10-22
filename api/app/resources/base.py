d9421c241c66cf3d55d11e0b61ed8548440699c6
Pablo <pskyvader@hotmail.com>
2019-10-22 15:32:52 -0500

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -1,20 +1,20 @@
-def init(method,params):
+def init(self,method,params):
     if len(params)>1:
         options=set(params[1:])
     else:
         options=()
     if method=='GET':
         if len(params)>0:
-            return get(params[0],options)
+            return self.get(params[0],options)
         else:
-            return get()
+            return self.get()
     elif len(params)>0:
         if method=='POST':
-            return post(params[0],options)
+            return self.post(params[0],options)
         elif method=='PUT':
-            return put(params[0],options)
+            return self.put(params[0],options)
         elif method=='DELETE':
-            return delete(params[0],options)
+            return self.delete(params[0],options)
     else:
         return {'error':404,'method':method,'params':params}
                 
