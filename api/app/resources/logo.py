f76d33f55b42153895ac462364c5ecf8dab401fa
Pablo <pskyvader@hotmail.com>
2019-10-22 15:17:22 -0500

    --- Auto-Git Commit ---

--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -1,13 +1,20 @@
 def init(self,method,params):
+    if len(params)>1:
+        options=set(params[1:])
+    else:
+        options=()
     if method=='GET':
-        return self.get(params[0],params[1:])
+        if len(params)>0:
+            return self.get(params[0],options)
+        else:
+            return self.get() 
     elif len(params)>0:
         if method=='POST':
-            return self.post(params[0],params[1:])
+            return self.post(params[0],options)
         elif method=='PUT':
-            return self.put(params[0],params[1:])
+            return self.put(params[0],options)
         elif method=='DELETE':
-            return self.delete(params[0],params[1:])
+            return self.delete(params[0],options)
     else:
         return {'error':404,'method':method,'params':params}
                 
