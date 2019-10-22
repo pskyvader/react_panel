08a585237f9c5d68a2dfdc5c0c1db24683142ead
Pablo <pskyvader@hotmail.com>
2019-10-22 13:44:34 -0500

    --- Auto-Git Commit ---

--- api/app/controllers/back/themes/paper/base.py
+++ api/app/controllers/back/themes/paper/base.py
@@ -7,8 +7,8 @@ from app.models.modulo import modulo as modulo_model
 from app.models.administrador import administrador as administrador_model
 from app.models.table import table as table_model
 
-from .lista import lista as lista_class
-from .detalle import detalle as detalle_class
+# from .lista import lista as lista_class
+# from .detalle import detalle as detalle_class
 
 import importlib
 import json
