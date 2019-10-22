394abbb6188e1cd2904cbcc424249f191411f07f
Pablo <pskyvader@hotmail.com>
2019-10-22 13:06:33 -0500

    --- Auto-Git Commit ---

--- api/app/controllers/back/themes/paper/header.py
+++ api/app/controllers/back/themes/paper/header.py
@@ -10,14 +10,13 @@ class header(base):
 
     def get(self):
         ret = {'body': []}
-        if 'ajax' not in app.post:
-            logo = logo_model.getById(3)
-            portada=image.portada(logo['foto'])
-            self.data['logo_max'] = image.generar_url(portada, 'panel_max')
-            logo = logo_model.getById(4)
-            portada=image.portada(logo['foto'])
-            self.data['logo_min'] = image.generar_url(portada, 'panel_min')
-            self.data['url_exit'] = functions.generar_url(['logout'], False)
-            self.data['date'] = functions.current_time()
-            ret['body'].append(('header', self.data))
+        logo = logo_model.getById(3)
+        portada=image.portada(logo['foto'])
+        self.data['logo_max'] = image.generar_url(portada, 'panel_max')
+        logo = logo_model.getById(4)
+        portada=image.portada(logo['foto'])
+        self.data['logo_min'] = image.generar_url(portada, 'panel_min')
+        self.data['url_exit'] = functions.generar_url(['logout'], False)
+        self.data['date'] = functions.current_time()
+        ret['body'].append(('header', self.data))
         return ret
