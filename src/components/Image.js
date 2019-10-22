26c6d9f800fb2f5e6f29babb5492165d8ef37333
Pablo <pskyvader@hotmail.com>
2019-10-22 17:14:11 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 const static_folder='public/images'
 function Image(props) {
     var final_image=props.image;
-    console.log(final_image[0],props.image[0]);
+    console.log(final_image[0]);
     var static_image=static_folder+final_image['folder']+final_image.parent+final_image.subfolder+final_image.url;
     return (
         <img className="" src={static_image} alt={props.title} />
