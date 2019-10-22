d1844d1343bf77450f7da47fd9de42907b7e0cdf
Pablo <pskyvader@hotmail.com>
2019-10-22 16:18:24 -0500

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ class Logo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            logo:''
+            logo:""
         };
     }
     componentDidMount() {
