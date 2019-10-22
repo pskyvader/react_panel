e208e31d2a02d12085b40e1e740a5edce5e6f49c
Pablo <pskyvader@hotmail.com>
2019-10-22 16:31:09 -0500

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -21,15 +21,3 @@ class base:
         else:
             return {'error':404,'method':method,'params':params}
                 
-    def get(self,id=0,**params):
-        return {'body':{}}
-
-    def post(self,id,**params):
-        return {'body':{}}
-        
-        
-    def put(self,id,**params):
-        return {'body':{}}
-        
-    def delete(self,id,**params):
-        return {'body':{}}
