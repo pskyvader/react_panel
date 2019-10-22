e002efe6944d626da64ee0d1959e0bd0b63c1d47
Pablo <pskyvader@hotmail.com>
2019-10-22 17:22:26 -0500

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,7 @@ const static_folder='public/images'
 function Image(props) {
     if (typeof(props.image)!=object){
         var image=props.image[0];
-        var static_image=static_folder+image['folder']+image.parent+image.subfolder+image.url;
+        var static_image=static_folder+image.folder+image.parent+image.subfolder+image.url;
     }else{
         var static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
     }
