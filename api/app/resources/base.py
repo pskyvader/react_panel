6e11d8386620ffd77530c32e89d82194cd5cf9b2
Pablo <pskyvader@hotmail.com>
2019-10-22 16:34:25 -0500

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -1,35 +1,36 @@
 class base:
     model=None
-    def init(self,method,params,model):
-        self.model=model
+    @classmethod
+    def init(cls,method,params,model):
+        cls.model=model
         if len(params)>1:
             options=set(params[1:])
         else:
             options=()
         if method=='GET':
             if len(params)>0:
-                return self.get(params[0],options)
+                return cls.get(params[0],options)
             else:
-                return self.get()
+                return cls.get()
         elif len(params)>0:
             if method=='POST':
-                return self.post(params[0],options)
+                return cls.post(params[0],options)
             elif method=='PUT':
-                return self.put(params[0],options)
+                return cls.put(params[0],options)
             elif method=='DELETE':
-                return self.delete(params[0],options)
+                return cls.delete(params[0],options)
         else:
             return {'error':404,'method':method,'params':params}
-                
-    def get(self,id=0,**params):
+    @classmethod 
+    def get(cls,id=0,**params):
         return {'body':{}}
-
-    def post(self,id,**params):
-        return {'body':{}}
-        
-        
-    def put(self,id,**params):
+    @classmethod
+    def post(cls,id,**params):
         return {'body':{}}
         
-    def delete(self,id,**params):
+    @classmethod
+    def put(cls,id,**params):
+        return {'body':{}}
+    @classmethod
+    def delete(cls,id,**params):
         return {'body':{}}
