6433cc4cf02d5f730e67f89bcb7fdd53c34108a1
Pablo <pskyvader@hotmail.com>
2019-12-03 15:15:04 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const static_folder = 'images'
 function Image(props) {
-    console.log(props.image);
     if (typeof (props.image) == 'object') {
         var image = props.image;
         if (image.length === 1) {
