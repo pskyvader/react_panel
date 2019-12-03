8f230a97c3d1eaf5319562c7e001511185ac09dc
Pablo <pskyvader@hotmail.com>
2019-12-03 15:22:04 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -7,8 +7,7 @@ function Image(props) {
         if (image.length === 1) {
             var static_image = [static_folder, image[0]].join("/");
         } else {
-            var multiple_images= Image_multiple(image, props.title);
-            return multiple_images;
+            return Image_multiple(image, props.title);
         }
     } else {
         static_image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
@@ -21,8 +20,8 @@ function Image(props) {
 function Image_multiple(images, title) {
     var image_list = [];
     images.forEach(i => {
-        var static_image = [static_folder, i].join("/"); 
-        image_list.push(<img className="" src={static_image} alt={title} />); 
+        var static_image = [static_folder, i].join("/");
+        image_list.push(<img className="" src={static_image} alt={title} />);
     });
 
     return image_list;
