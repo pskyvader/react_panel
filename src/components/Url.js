8434b02affe5e1c373c4fcbb81a41484028021f0
Pablo <pskyvader@hotmail.com>
2019-12-03 13:16:48 -0600

    --- Auto-Git Commit ---

--- src/components/Url.js
+++ src/components/Url.js
@@ -1,11 +1,9 @@
 function Url(...args) {
     var final_url = '';
-    args.forEach(
-        function(e, k) {
-            if (k > 0) final_url += '/';
-            final_url += e;
-        }
-    );
+    args.forEach(function(e, k) {
+        if (k > 0) final_url += '/';
+        final_url += e;
+    });
     return final_url;
 }
 export default Url;
