f98025f8be9ae40344e6467ab09898466597821d
Pablo <pskyvader@hotmail.com>
2019-12-03 14:34:32 -0600

    --- Auto-Git Commit ---

--- api/app/resources/base.py
+++ api/app/resources/base.py
@@ -33,3 +33,28 @@ class base:
         
     def delete(self,id,*params):
         return {'body':{}}
+
+        
+    @classmethod
+    def process_image(cls,images,options):
+        recortes=image.get_recortes(cls.model.__name__)
+        recortes=[x['tag'] for x in recortes]
+
+        url_list=[]
+        if "portada" in options:
+            portada = image.portada(images)
+            if len(options)>1:
+                if options[1] in recortes:
+                    url_list=[image.generar_url(portada, options[1])]
+            else:
+                for recorte in recortes:
+                    url_list.append(image.generar_url(portada, recorte))
+        elif len(options)>0:
+            if options[1] in recortes:
+                for i in images:
+                    url_list.append(image.generar_url(i, options[1]))
+        else:
+            for i in images:
+                for recorte in recortes:
+                    url_list.append(image.generar_url(i, recorte))
+        return url_list
--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -18,29 +18,6 @@ class logo(base):
             if 'foto' in data:
                 data['foto']=cls.process_image(data['foto'],options)
         return {"body": data}
-    @classmethod
-    def process_image(cls,images,options):
-        recortes=image.get_recortes(cls.model.__name__)
-        recortes=[x['tag'] for x in recortes]
-
-        url_list=[]
-        if "portada" in options:
-            portada = image.portada(images)
-            if len(options)>1:
-                if options[1] in recortes:
-                    url_list=[image.generar_url(portada, options[1])]
-            else:
-                for recorte in recortes:
-                    url_list.append(image.generar_url(portada, recorte))
-        elif len(options)>0:
-            if options[1] in recortes:
-                for i in images:
-                    url_list.append(image.generar_url(i, options[1]))
-        else:
-            for i in images:
-                for recorte in recortes:
-                    url_list.append(image.generar_url(i, recorte))
-        return url_list
 
     @classmethod
     def post(cls, id, *options):
