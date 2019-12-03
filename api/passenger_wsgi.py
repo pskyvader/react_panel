9a9716b643efe98d1c82cf12ea01246ae53f58c6
Pablo <pskyvader@hotmail.com>
2019-12-03 14:43:17 -0600

    --- Auto-Git Commit ---

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -19,9 +19,10 @@ def application2(environ, start_response):
 
     if isinstance(ret, str):
         if ret != "":
-            ret = compress(bytes(ret, "utf-8"))
-            main_data["headers"].append(("Accept-encoding", "gzip,deflate"))
-            main_data["headers"].append(("Content-Encoding", "gzip"))
+            ret = bytes(ret, "utf-8")
+            #ret = compress(bytes(ret, "utf-8"))
+            #main_data["headers"].append(("Accept-encoding", "gzip,deflate"))
+            #main_data["headers"].append(("Content-Encoding", "gzip"))
         else:
             ret = b""
 
