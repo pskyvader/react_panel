fc7f16f362c5154c577eb8838c16d0e2a97bc2d5
Pablo <pskyvader@hotmail.com>
2019-12-03 14:45:03 -0600

    --- Auto-Git Commit ---

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -15,7 +15,7 @@ def application2(environ, start_response):
     ret = main_data["response_body"]
 
     if not isinstance(ret,str):
-        ret=json.dumps(ret, indent=2)
+        ret=json.dumps(ret, indent=0)
 
     if isinstance(ret, str):
         if ret != "":
