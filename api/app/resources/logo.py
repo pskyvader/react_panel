8c98282e840ec89f97a75b58ad46e19de0f84b15
Pablo <pskyvader@hotmail.com>
2019-12-03 13:20:03 -0600

    --- Auto-Git Commit ---

--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -1,26 +1,32 @@
 from .base import base
 from app.models.logo import logo as logo_model
+from core.image import image
 
-def init(method,params):
-    l=logo()
-    return l.init(method,params,logo_model)
+
+def init(method, params):
+    l = logo()
+    return l.init(method, params, logo_model)
+
 
 class logo(base):
-    def get(self,id=0,*options):
-        if id==0:
-            data=self.model.getAll()
+    def get(self, id=0, *options):
+        if id == 0:
+            data = self.model.getAll()
         else:
-            data=self.model.getById(id)
-        return {'body':data}
-    @classmethod
-    def post(cls,id,*options):
-        return {'body':{}}
-        
-    @classmethod
-    def put(cls,id,*options):
-        return {'body':{}}
-    @classmethod
-    def delete(cls,id,*options):
-        return {'body':{}}
+            data = self.model.getById(id)
+        if "portada" in options:
+            data = image.portada(data)
+        return {"body": data}
 
+    @classmethod
+    def post(cls, id, *options):
+        return {"body": {}}
+
+    @classmethod
+    def put(cls, id, *options):
+        return {"body": {}}
+
+    @classmethod
+    def delete(cls, id, *options):
+        return {"body": {}}
 
