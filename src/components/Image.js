3af9af4319fdae2e2fedba2e9e2f40b4c8575273
Pablo <pskyvader@hotmail.com>
2019-12-03 14:49:32 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,9 +3,10 @@ import React from 'react';
 const static_folder='images'
 function Image(props) {
     if (typeof(props.image)=='object'){
-        console.log(props.image);
-        var image=props.image[0];
-        var static_image=[static_folder,image.folder,image.parent,image.subfolder,image.url].join("/");
+        var image=props.image;
+        if (image.length==1){
+            var static_image=[static_folder,image[0]].join("/");
+        }
     }else{
         static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
     }
