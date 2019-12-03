8d6d3ff2257719d9ec1e4a6d6fd754a0aaa253cb
Pablo <pskyvader@hotmail.com>
2019-12-03 14:22:02 -0600

    --- Auto-Git Commit ---

--- api/app/resources/logo.py
+++ api/app/resources/logo.py
@@ -20,8 +20,8 @@ class logo(base):
         return {"body": data}
     @classmethod
     def process_image(cls,images,options):
-        print(cls.model.__name__)
         recortes=image.get_recortes(cls.model.__name__)
+        print(recortes)
         recortes=[x for x['tag'] in recortes]
 
         url_list=[]
