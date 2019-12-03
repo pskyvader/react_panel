bc4f69aea46446cc225b5c7c03530091049d689e
Pablo <pskyvader@hotmail.com>
2019-12-03 14:58:03 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
-const static_folder='images'
+const static_folder = 'images'
 function Image(props) {
-    if (typeof(props.image)=='object'){
-        var image=props.image;
-        if (image.length===1){
-            var static_image=[static_folder,image[0]].join("/");
+    if (typeof (props.image) == 'object') {
+        var image = props.image;
+        if (image.length === 1) {
+            var static_image = [static_folder, image[0]].join("/");
+        } else {
+            return Image_multiple(props.image, props.title);
         }
-    }else{
-        static_image='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
+    } else {
+        static_image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
     }
     return (
         <img className="" src={static_image} alt={props.title} />
     );
 }
+
+function Image_multiple(images, title) {
+    return (
+        images.forEach(i => {
+            var static_image = [static_folder, i].join("/");
+            <img className="" src={static_image} alt={title} />
+        }
+        );
+    );
+}
+
+
 export default Image;
