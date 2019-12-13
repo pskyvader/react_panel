cb2340d5182512ba8072950edbef2c02e9132fce
Pablo <pskyvader@hotmail.com>
2019-12-13 15:57:48 -0600

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,12 +5,6 @@ import Url from './Url';
 class Logo extends Component {
     resource = 'logo';
     sub = 'portada';
-    constructor(props) {
-        super(props);
-        this.id = props.id;
-        this.size = props.size;
-        console.log('construct');
-    }
     componentDidMount() {
         console.log('mount');
         this.get_logo();
