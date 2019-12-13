bee266f755f7ffe6e51b4f86dedc0a3d1b37fb1e
Pablo <pskyvader@hotmail.com>
2019-12-13 15:59:18 -0600

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,11 +5,12 @@ import Url from './Url';
 class Logo extends Component {
     resource = 'logo';
     sub = 'portada';
+    foto='';
+    title='';
     constructor(props) {
         super(props);
         this.id = props.id;
         this.size = props.size;
-        console.log('construct');
     }
     componentDidMount() {
         console.log('mount');
@@ -18,21 +19,18 @@ class Logo extends Component {
 
 
     get_logo() {
-        if (!this.state || this.state.foto === '') {
+        if (this.foto === '') {
             fetch(Url(this.resource, this.id, this.sub, this.size))
                 .then(response => response.json())
                 .then(data => {
-                    this.setState({ foto: data.foto, title: data.titulo });
+                    this.foto=data.foto;
+                    this.title= data.titulo;
                 });
-                // .then(data => {
-                //     this.foto=data.foto;
-                //     this.title= data.titulo;
-                // });
         }
     }
 
     render() {
-        if (this.state && this.state.foto !== '') {
+        if (this.foto !== '') {
             console.log(this,this.state.foto,'ok');
             return (
                 <Image image={this.state.foto} title={this.state.title} />
