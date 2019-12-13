91a4ca4da699769f0cf1333c70590af646e250ed
Pablo <pskyvader@hotmail.com>
2019-12-13 15:22:17 -0600

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react';
 import Image from './Image';
 import Url from './Url';
 
-const API = process.env.REACT_APP_API_URL;
 class Logo extends Component {
     resource = 'logo';
     sub = 'portada';
-    foto = '';
-    title = '';
     constructor(props) {
         super(props);
         this.id = props.id;
         this.size = props.size;
+        this.state = {
+            logo: "",
+            title: "",
+        };
     }
     componentDidMount() {
-        console.log('asdf',this,typeof(this.foto));
-        if (this.foto === '') {
-            fetch(Url(API, this.resource, this.id, this.sub, this.size))
+        this.get_logo();
+    }
+
+
+    get_logo() {
+        if (this.state.logo === '') {
+            fetch(Url(this.resource, this.id, this.sub, this.size))
                 .then(response => response.json())
                 .then(data => {
-                    this.foto = data.foto;
-                    this.title = data.titulo;
+                    this.setState({ logo: data.foto, title: data.titulo });
                 });
-        }else{
-            console.log(this.foto);
         }
-        this.render();
     }
 
     render() {
         return (
-            <Image image={this.foto} title={this.title} />
+            <Image image={this.state.logo} title={this.state.title} />
         )
     }
 }
