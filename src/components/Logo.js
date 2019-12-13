269de57ac48e20e421061671fc5d1bff719a6c5b
Pablo <pskyvader@hotmail.com>
2019-12-13 15:39:02 -0600

    --- Auto-Git Commit ---

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,15 +5,15 @@ import Url from './Url';
 class Logo extends Component {
     resource = 'logo';
     sub = 'portada';
-    state = {
-        foto: "",
-        title: "",
-    };
     constructor(props) {
         super(props);
         this.id = props.id;
         this.size = props.size;
     }
+    componentDidMount() {
+        this.get_logo();
+    }
+
 
     get_logo() {
         if (this.state.foto === '') {
