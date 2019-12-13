a152b89b06d52bf4d3144aa5002d79b381b0ffef
Pablo <pskyvader@hotmail.com>
2019-12-13 14:20:46 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -20,8 +20,8 @@ function Image(props) {
 
 function Image_multiple(images, title) {
     var image_list = [];
-    console.log(images);
     images.forEach(i => {
+        console.log(i);
         var static_image = [static_folder, i].join("/");
         image_list.push(<img className="" src={static_image} alt={title} />);
     });
