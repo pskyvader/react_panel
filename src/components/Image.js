20b07c84da67d137cfcc2a5a7e3237419a1b33c2
Pablo <pskyvader@hotmail.com>
2019-12-13 14:17:00 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,7 @@ function Image(props) {
     if (typeof (props.image) == 'object') {
         var image = props.image;
         if (image.length === 1) {
-            console.log(image[0],Object.values(image[0]));
+            console.log(image[0],Object.values(image[0])[0]);
             var static_image = [static_folder, image[0]].join("/");
         } else {
             return Image_multiple(image, props.title);
