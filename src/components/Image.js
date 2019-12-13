3c8c17cec259c6078d604029d52e0841d99de956
Pablo <pskyvader@hotmail.com>
2019-12-13 14:30:16 -0600

    --- Auto-Git Commit ---

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,29 +4,28 @@ const static_folder = 'images'
 function Image(props) {
     if (typeof (props.image) == 'object') {
         var image = props.image;
-        if (image.length === 0) {
-            image = Object.values(image[0])[0];
-            var static_image = [static_folder, image].join("/");
+        if (image.length === 1) {
+            return single_image(image[0], props.title);
         } else {
             return Image_multiple(image, props.title);
         }
     } else {
         static_image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
+        return (single_image(static_image, props.title));
     }
-    return (single_image(static_image, props.title));
 }
 
 function Image_multiple(images, title) {
     var image_list = [];
     images.forEach(i => {
-        var image = Object.values(i)[0];
-        var static_image = [static_folder, image].join("/");
-        image_list.push(single_image(static_image, title));
+        image_list.push(single_image(i, title));
     });
     return image_list;
 }
 
-function single_image(static_image, title) {
+function single_image(image, title) {
+    image = Object.values(image)[0];
+    var static_image = [static_folder, image].join("/");
     return <img className="" src={static_image} alt={title} />
 }
 
