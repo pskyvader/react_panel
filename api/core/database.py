5df84a1ada614f55f83867eab3a1055a6fc58e52
Pablo <pskyvader@hotmail.com>
2019-12-13 13:42:54 -0600

    --- Auto-Git Commit ---

--- api/core/database.py
+++ api/core/database.py
@@ -78,8 +78,8 @@ class database:
                             r[k] = v.strftime("%Y-%m-%d %H:%M:%S")
                     for k, v in enumerate(list(r.values())):
                         r[k] = v
-                    
-                    print(r)
+                
+                print(rows)
             else:
                 self._connection.commit()
                 self.last_insert_id = cursor.lastrowid
