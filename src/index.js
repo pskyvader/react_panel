62e2cf8748809b304334c55aa18cf2a27e314cc4
Pablo <pskyvader@hotmail.com>
2019-12-13 15:08:02 -0600

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const API = React.createContext('http://localhost:8080');
-
 ReactDOM.render(
     <BrowserRouter>
         <App />
