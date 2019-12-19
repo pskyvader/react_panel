b5f187ade69f35f3c2c8702ccb782fd52df747ab
Pablo <pskyvader@hotmail.com>
2019-12-19 12:36:20 -0600

    --- Auto-Git Commit ---

--- api/config/bdd/a.py
+++ api/config/bdd/a.py
@@ -4,8 +4,7 @@ folder=os.path.dirname(__file__)+'\\'
 file_str=folder+'bdd.json'
 
 with open(file_str,'r') as file1:
-    print(file1)
-    tables=json.loads(file1)
+    tables=json.loads(file1.read())
     for t in tables:
         with open(folder+t['tablename']+'.json','w') as table:
             table.write(json.dumps(t))
