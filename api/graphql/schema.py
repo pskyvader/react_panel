d4aa68cd70e5993fd2717783f89c42ecd8e1c7b6
Pablo <pskyvader@hotmail.com>
2019-12-19 12:13:35 -0600

    --- Auto-Git Commit ---

---
+++ api/graphql/schema.py
@@ -0,0 +1,11 @@
+import graphene
+
+class Character(graphene.Interface):
+    id = graphene.ID()
+    name = graphene.String()
+    friends = graphene.List(lambda: Character)
+    appears_in = graphene.List(Episode)
+
+    def resolve_friends(self, info):
+        # The character friends is a list of strings
+        return [get_character(f) for f in self.friends]
