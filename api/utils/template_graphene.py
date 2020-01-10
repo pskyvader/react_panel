f8b1add5fe2a284934d56a88e4564bddcd2185b2
Pablo <pskyvader@hotmail.com>
2020-01-10 15:48:05 -0600

    --- Auto-Git Commit ---

--- api/utils/template_graphene.py
+++ api/utils/template_graphene.py
@@ -1,4 +1,4 @@
 class TABLENAME_model(Base):
     __tablename__ = "PREFIX_TABLENAME"
-    IDNAME = Column(Integer, primary_key=True)
+    IDNAME = Column(Integer, primary_key=True, nullable=False)
     FIELDS
