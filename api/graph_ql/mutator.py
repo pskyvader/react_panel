8ea50f5a25e07fca656ee27bee1b90d0bc4a338a
Pablo <pskyvader@hotmail.com>
2020-01-10 16:32:11 -0600

    --- Auto-Git Commit ---

--- api/graph_ql/mutator.py
+++ api/graph_ql/mutator.py
@@ -1,9 +1,16 @@
-def input_to_dictionary(input):
-    """Method to convert Graphene inputs into dictionary"""
+def input_to_dictionary(input_variable,id_key):
+    """Method to convert Graphene input_variables into dictionary"""
     dictionary = {}
-    for key in input:
+    for key in input_variable:
         # Convert GraphQL global id to database id
-        if key[-2:] == 'id':
-            input[key] = from_global_id(input[key])[1]
-        dictionary[key] = input[key]
+        if key[-2:] == id_key:
+            input_variable[key] = from_global_id(input_variable[key])[1]
+        dictionary[key] = input_variable[key]
     return dictionary
+
+def mutation_create(table_model):
+    data = utils.input_to_dictionary(input)
+    table = table_model(**data)
+    db_session.add(table)
+    db_session.commit()
+    return table
