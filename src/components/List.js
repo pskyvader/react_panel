1b483c3babff6f815d12fe67589b3b9247e9d9aa
Pablo <pskyvader@hotmail.com>
2020-01-10 19:23:45 -0600

    --- Auto-Git Commit ---

--- src/components/List.js
+++ src/components/List.js
@@ -147,28 +147,29 @@ MuiVirtualizedTable.propTypes = {
 const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
 
-const sample = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
-];
+// const sample = [
+//     ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+//     ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+//     ['Eclair', 262, 16.0, 24, 6.0],
+//     ['Cupcake', 305, 3.7, 67, 4.3],
+//     ['Gingerbread', 356, 16.0, 49, 3.9],
+// ];
 
-function createData(id, dessert, calories, fat, carbs, protein) {
-    return { id, dessert, calories, fat, carbs, protein };
-}
+// function createData(id, dessert, calories, fat, carbs, protein) {
+//     return { id, dessert, calories, fat, carbs, protein };
+// }
 
-const rows = [];
+// const rows = [];
 
-for (let i = 0; i < 200; i += 1) {
-    const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-    rows.push(createData(i, ...randomSelection));
-}
+// for (let i = 0; i < 200; i += 1) {
+//     const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+//     rows.push(createData(i, ...randomSelection));
+// }
 
 
 
 export default function List(props) {
+    const table='all' + props.table;
 
     const GET_LIST = gql(`
     query get_list{
@@ -180,14 +181,24 @@ export default function List(props) {
             }
         }
     }
-    `.replace('$table','all' + props.table).replace('$fields',props.fields) );
+    `.replace('$table',table).replace('$fields',props.fields) );
 
 
-    const variables = { variables: { table: 'all' + props.table }, }
-    const { loading, error, data } = useQuery(GET_LIST, variables);
+
+    // const variables = { variables: { table: table }, }
+    // const { loading, error, data } = useQuery(GET_LIST, variables);
+    const { loading, error, data } = useQuery(GET_LIST);
     if (loading) return '...';
     if (error) return 'Error';
-    console.log(data);
+    //console.log(data[table]);
+
+    const rows = [];
+    
+    data[table]['edges'].forEach(element => {
+        const e = element['node'];
+        rows.push(e);
+    });
+
 
     return (
         <Paper style={{ height: 400, width: '100%' }}>
