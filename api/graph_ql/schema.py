7111f272a0afcf8b99574f5e89a8ec2ef5291de7
Pablo <pskyvader@hotmail.com>
2020-01-21 20:05:23 -0600

    --- Auto-Git Commit ---

--- api/graph_ql/schema.py
+++ api/graph_ql/schema.py
@@ -2,103 +2,45 @@ from .schemas import *
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
-
-
 import sys, inspect
-clsmembers = inspect.getmembers(sys.modules['graph_ql.schemas'])
 
-schema_list=[]
+# Add schemas to list
+clsmembers = inspect.getmembers(sys.modules["graph_ql.schemas"])
+schema_list = []
 for cl in clsmembers:
-    if 'schema' in cl[0]:
+    if "schema" in cl[0]:
         schema_list.append(cl)
-
 
 
 class attributes_query:
     pass
 
+
 class attributes_mutation:
     pass
 
-for title,sc in schema_list:
-    title=title[:-7]
+
+types = []
+
+# add schemas properties to query and mutation
+for title, sc in schema_list:
+    title = title[:-7]
     for property, value in vars(sc).items():
-        if property==title:
+        if property in [ title, "resolve_" + title, "all_" + title, "resolve_all_" + title, ]:
             setattr(attributes_query, property, value)
-        elif property=='resolve_'+title:
-            setattr(attributes_query, 'resolve_'+title, value)
-        elif property=='all_'+title:
-            setattr(attributes_query, 'all_'+title, value)
-        elif property=='resolve_all_'+title:
-            setattr(attributes_query, 'resolve_all_'+title, value)
-        elif property=='create_'+title:
-            setattr(attributes_mutation, 'create_'+title, value.Field())
-        elif property=='update_'+title:
-            setattr(attributes_mutation, 'update_'+title, value.Field())
-        elif property=='delete_'+title:
-            setattr(attributes_mutation, 'delete_'+title, value.Field())
+        elif property in ["create_" + title, "update_" + title, "delete_" + title]:
+            setattr(attributes_mutation, property, value.Field())
+        elif "_schema" in property:
+            types.append(value)
 
-        
 
-class Query(graphene.ObjectType,attributes_query):
+class Query(graphene.ObjectType, attributes_query):
     node = relay.Node.Field()
 
 
-    
-class Mutation(graphene.ObjectType,attributes_mutation):
+class Mutation(graphene.ObjectType, attributes_mutation):
     pass
-    
-    
-    
-    
-    
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-# __TYPES__
-    
-schema = graphene.Schema(query=Query,mutation=Mutation)
-# __TYPES__
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+schema = graphene.Schema(query=Query, mutation=Mutation, types=types)
+
