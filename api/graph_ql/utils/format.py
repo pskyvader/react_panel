ba58484516b5d4875462428e8070543181196171
Pablo <pskyvader@hotmail.com>
2020-01-22 13:57:51 -0600

    --- Auto-Git Commit ---

--- api/graph_ql/utils/format.py
+++ api/graph_ql/utils/format.py
@@ -6,7 +6,6 @@ def parse_post(environ, buffer):
     p = FieldStorage(fp=BytesIO(buffer), environ=environ, keep_blank_values=True)
     if p.list != None:
         post = post_field(p)
-
     return post
 
 
@@ -17,11 +16,7 @@ def post_field(p):
     post = {}
     try:
         for key in p.keys():
-            if (
-                isinstance(p[key], FieldStorage)
-                and p[key].file
-                and p[key].filename != None
-            ):
+            if ( isinstance(p[key], FieldStorage) and p[key].file and p[key].filename != None ):
                 if not "file" in post:
                     post["file"] = []
                 tmpfile = p[key].file.read()
@@ -30,9 +25,7 @@ def post_field(p):
                 if mime == None:
                     mime = "text/plain"
                 post["file"].append({"name": name, "type": mime, "tmp_name": tmpfile})
-            elif isinstance(p[key], MiniFieldStorage) or isinstance(
-                p[key], FieldStorage
-            ):
+            elif isinstance(p[key], MiniFieldStorage) or isinstance( p[key], FieldStorage ):
                 post[key] = p[key].value
             elif isinstance(p[key], list):
                 tmp_list = []
@@ -45,7 +38,6 @@ def post_field(p):
             else:
                 post[key] = p[key]
     except Exception as error:
-        # print('Error al obtener post: ' + repr(error) + repr(p)
         raise RuntimeError("Error al obtener post: " + repr(error) + repr(p))
     return post
 
@@ -54,19 +46,21 @@ def get_content_type_by_filename(file_name):
     from os.path import basename
 
     mime_type = ""
-    mime_map = {}
-    mime_map["js"] = "application/javascript"
-    mime_map["xlsx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    mime_map["xltx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.template"
-    mime_map["potx"] = "application/vnd.openxmlformats-officedocument.presentationml.template"
-    mime_map["ppsx"] = "application/vnd.openxmlformats-officedocument.presentationml.slideshow"
-    mime_map["pptx"] = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    mime_map["sldx"] = "application/vnd.openxmlformats-officedocument.presentationml.slide"
-    mime_map["docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    mime_map["dotx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.template"
-    mime_map["xlam"] = "application/vnd.ms-excel.addin.macroEnabled.12"
-    mime_map["xlsb"] = "application/vnd.ms-excel.sheet.binary.macroEnabled.12"
-    mime_map["apk"] = "application/vnd.android.package-archive"
+    mime_map = {
+        "js": "application/javascript"
+        "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        "xltx": "application/vnd.openxmlformats-officedocument.spreadsheetml.template"
+        "potx": "application/vnd.openxmlformats-officedocument.presentationml.template"
+        "ppsx": "application/vnd.openxmlformats-officedocument.presentationml.slideshow"
+        "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        "sldx": "application/vnd.openxmlformats-officedocument.presentationml.slide"
+        "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        "dotx": "application/vnd.openxmlformats-officedocument.wordprocessingml.template"
+        "xlam": "application/vnd.ms-excel.addin.macroEnabled.12"
+        "xlsb": "application/vnd.ms-excel.sheet.binary.macroEnabled.12"
+        "apk": "application/vnd.android.package-archive"
+    }
+    
     try:
         suffix = ""
         name = basename(file_name)
