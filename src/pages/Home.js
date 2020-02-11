e5a5c80931082b2e9e0b0005f76305c00f554b23
Pablo <pskyvader@hotmail.com>
2020-02-11 15:46:57 -0600

    --- Auto-Git Commit ---

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,20 @@ import {
 } from '@material-ui/core';
 import List from '../components/List';
 import Form from '../components/Form';
+import Box from '@material-ui/core/Box';
 
 export default () => (
     <React.Fragment>
         <Typography variant="h3">Welcome Home!</Typography>
-        <List table="Igaccounts" fields={['idigaccounts' ,'fullName' ,'username' ,'profilePicUrl' ,'followerCount' ,'followingCount' ,'fecha' ,'following' ,'follower' ,'favorito' ,'hashtag']}></List>
-        <Form></Form>
+        <Box component="div" display="flex" p={1} m={1} bgcolor="background.paper" justifyContent="center">
+            <Form></Form>
+        </Box>
+
+        <Box component="div" display="flex" p={1} m={1} bgcolor="background.paper">
+            <List table="Igaccounts" fields={['idigaccounts', 'fullName', 'username', 'profilePicUrl', 'followerCount', 'followingCount', 'fecha', 'following', 'follower', 'favorito', 'hashtag']}></List>
+
+        </Box>
+
+
     </React.Fragment>
 );
