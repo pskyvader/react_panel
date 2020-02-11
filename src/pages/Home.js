50ed44f2b886c0f68f47e5234f78bcbf46774e9f
Pablo <pskyvader@hotmail.com>
2020-02-11 15:36:52 -0600

    --- Auto-Git Commit ---

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import {
     Typography,
 } from '@material-ui/core';
 import List from '../components/List';
+import Form from '../components/Form';
 
 export default () => (
     <React.Fragment>
         <Typography variant="h3">Welcome Home!</Typography>
         <List table="Igaccounts" fields={['idigaccounts' ,'fullName' ,'username' ,'profilePicUrl' ,'followerCount' ,'followingCount' ,'fecha' ,'following' ,'follower' ,'favorito' ,'hashtag']}></List>
+        <Form></Form>
     </React.Fragment>
 );
