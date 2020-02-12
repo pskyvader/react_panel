85b62f11ffa34e17a91686e79036b479ec125486
Pablo <pskyvader@hotmail.com>
2020-02-12 15:02:13 -0600

    --- Auto-Git Commit ---

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
-import { useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 
 import Hidden from '@material-ui/core/Hidden';
@@ -24,58 +14,27 @@ import Hidden from '@material-ui/core/Hidden';
 
 import Logo from './Logo';
 import useStyles from './Styles';
+import Sidebar from './Sidebar';
 
 
 
 
 
-export default function PersistentDrawerLeft(props) {
+export default function Header(props) {
     const classes = useStyles();
-    const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
     const handleDrawer = () => {
         setOpen(!open);
     }
-    const toggleDrawer = (side, open) => event => {
+    const toggleDrawer = (open) => event => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
         }
-
         setOpen(open);
     };
 
 
-    const sideList = () => (
-        <div
-            className={classes.list}
-            role="presentation"
-        >
-            <div className={classes.drawerHeader}>
-                <IconButton onClick={handleDrawer}>
-                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                </IconButton>
-            </div>
-            <Divider />
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-    );
 
     return (
         <div className={classes.root}>
@@ -89,15 +48,16 @@ export default function PersistentDrawerLeft(props) {
                     <Typography variant="h6" noWrap> Persistent drawer </Typography>
                 </Toolbar>
             </AppBar>
+            
             <Hidden smDown>
                 <Drawer className={classes.drawer} variant="persistent" open={open} classes={{ paper: classes.drawerPaper, }}>
-                    <sideList/>
+                    <Sidebar classes={classes} handleDrawer={handleDrawer}/>
                 </Drawer>
             </Hidden>
 
             <Hidden mdUp>
                 <Drawer variant="temporary" open={open} classes={{ paper: classes.drawerPaper, }} onClose={toggleDrawer(false)}>
-                <sideList/>
+                    <Sidebar classes={classes} handleDrawer={handleDrawer}/>
                 </Drawer>
             </Hidden>
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import IconButton from '@material-ui/core/IconButton';
+import { useTheme } from '@material-ui/core/styles';
+
+
+function Sidebar(props){
+    const classes=props.classes;
+    const handleDrawer=props.handleDrawer;
+    const theme = useTheme();
+
+    return (
+        <div
+            className={classes.list}
+            role="presentation"
+        >
+            <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawer}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+}
+
+export default Sidebar;
