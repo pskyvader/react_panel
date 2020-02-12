b9df070c0f228c82cfbc04d0eb67636b21a8dce3
Pablo <pskyvader@hotmail.com>
2020-02-12 13:41:29 -0600

    --- Auto-Git Commit ---

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import React, { Fragment } from 'react';
-import { CssBaseline, withStyles, } from '@material-ui/core';
+import React from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import {
-    Route,
-    Switch,
-    useLocation
-} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_API_URL,
-});
-
-
-const styles = theme => ({
-    main: {
-        padding: theme.spacing(3),
-        [theme.breakpoints.down('xs')]: {
-            padding: theme.spacing(2),
-        },
-    },
 });
 
 
@@ -29,22 +14,19 @@ const styles = theme => ({
 
 const App = () => (
     <ApolloProvider client={client}>
-        <Fragment>
-            <Header>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
+        <Header>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/home">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    404 not found
                     </Route>
-                    <Route path="/home">
-                        <Home />
-                    </Route>
-                    <Route path="*">
-                        404 not found
-                    </Route>
-                </Switch>
-            </Header>
-        </Fragment>
+            </Switch>
+        </Header>
     </ApolloProvider>
 );
-// export default withStyles(styles)(App);
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {useTheme} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -20,69 +20,20 @@ import MailIcon from '@material-ui/icons/Mail';
 
 
 import Logo from './Logo';
+import useStyles from './Styles';
 
-const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-}));
+
+
 
 export default function PersistentDrawerLeft(props) {
     const classes = useStyles();
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
+
+    const handleDrawer=()=>{
+        setOpen(!open);
+    }
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -97,7 +48,7 @@ export default function PersistentDrawerLeft(props) {
             <CssBaseline />
             <AppBar position="fixed" className={clsx(classes.appBar, { [classes.appBarShift]: open, })} >
                 <Toolbar>
-                    <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start" className={clsx(classes.menuButton, open && classes.hide)} >
+                    <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawer} edge="start" className={clsx(classes.menuButton, open && classes.hide)} >
                         <MenuIcon />
                     </IconButton>
                     <Logo id='2' width="100" height="40" />
@@ -108,12 +59,7 @@ export default function PersistentDrawerLeft(props) {
 
 
             <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{ paper: classes.drawerPaper, }} >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </div>
-                <Divider />
+                <div className={classes.drawerHeader}/>
                 <List>
                     {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                         <ListItem button key={text}>
@@ -132,7 +78,7 @@ export default function PersistentDrawerLeft(props) {
                     ))}
                 </List>
             </Drawer>
-            
+
             <main className={clsx(classes.content, { [classes.contentShift]: open, })} >
                 <div className={classes.drawerHeader} />
                 {props.children}
