fa275f1018f99aa40d9c9a4b476638549d837882
Pablo <pskyvader@hotmail.com>
2020-02-12 12:01:00 -0600

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,41 +4,62 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+    Route,
+    Switch,
+    useLocation
+} from "react-router-dom";
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
+    uri: process.env.REACT_APP_API_URL,
 });
 
 
 const styles = theme => ({
-  main: {
-    padding: theme.spacing(3),
-    [theme.breakpoints.down('xs')]: {
-      padding: theme.spacing(2),
+    main: {
+        padding: theme.spacing(3),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(2),
+        },
     },
-  },
 });
 
 
 
 
 const App = ({ classes }) => (
-  <ApolloProvider client={client}>
-    <Fragment>
-      <CssBaseline />
-      <Header />
-      <main className={classes.main}>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/home">
-        <Home />
-      </Route>
-      
-      </main>
-    </Fragment>
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+        <Fragment>
+            <CssBaseline />
+            <Header />
+            <main className={classes.main}>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/home">
+                        <Home />
+                    </Route>
+                    <Route path="*">
+                        <NoMatch />
+                    </Route>
+                </Switch>
+            </main>
+        </Fragment>
+    </ApolloProvider>
 );
+
+
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
+}
 
 export default withStyles(styles)(App);
