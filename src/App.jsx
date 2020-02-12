cf663cd36dc9e8401647e472ba6fe38ef2401fb4
Pablo <pskyvader@hotmail.com>
2020-02-12 13:11:13 -0600

    --- Auto-Git Commit ---

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { CssBaseline, withStyles, } from '@material-ui/core';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL,
+});
+
+
+const styles = theme => ({
+  main: {
+    padding: theme.spacing(3),
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(2),
+    },
+  },
+});
+
+
+
+
+const App = ({ classes }) => (
+  <ApolloProvider client={client}>
+    <Fragment>
+      <CssBaseline />
+      <Header />
+      <Sidebar/>
+
+      <main className={classes.main}>
+        <Home />
+      </main>
+    </Fragment>
+  </ApolloProvider>
+);
+
+export default withStyles(styles)(App);
