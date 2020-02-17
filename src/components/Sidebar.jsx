682708684e544c479e28a839054baea78d4690f6
Pablo <pskyvader@hotmail.com>
2020-02-17 16:16:57 -0600

    --- Auto-Git Commit ---

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Local_storage from './Local_storage';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import ErrorLink from './ErrorLink';
-import { Link,useRouteMatch } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 
 import { CircularProgress, ListItem, ListItemIcon, ListItemText, Divider, List, IconButton, Hidden, Drawer } from '@material-ui/core';
 import { ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon, MoveToInbox as InboxIcon, Mail as MailIcon } from '@material-ui/icons';
@@ -11,7 +11,15 @@ import { ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon, MoveT
 
 
 
-const sideList = (classes, handleDrawer, theme, final_list,path,url) => (
+const child_button = (element, hijo, url) => (
+    <ListItem button component={Link} to={`${url}/${element.module}`} key={element.module + element.orden + hijo.tipo}>
+        
+
+        <ListItemText primary={hijo.titulo} />
+    </ListItem>
+)
+
+const sideList = (classes, handleDrawer, theme, final_list, path, url) => (
     <div className={classes.list} role="presentation" >
         <div className={classes.drawerHeader}>
             <IconButton onClick={handleDrawer}>
@@ -22,7 +30,7 @@ const sideList = (classes, handleDrawer, theme, final_list,path,url) => (
         {final_list.map((sublist, index) => (
             <Fragment key={'sidebar_list' + index}>
                 <List>
-                    {sublist.map((element) => {
+                    {sublist.forEach((element) => {
                         if (element.module === 'separador') {
                             return (
                                 <ListItem key={element.module + element.orden}>
@@ -30,13 +38,11 @@ const sideList = (classes, handleDrawer, theme, final_list,path,url) => (
                                 </ListItem>
                             )
                         } else {
-                            return (
-                                <ListItem button component={Link} to={`${url}/${element.module}`} key={element.module + element.orden}>
-                                    <ListItemIcon>{element.orden % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                    <ListItemText primary={element.titulo} />
-                                </ListItem>
-                            );
-
+                            if (element.hijo.length === 1) {
+                                return child_button(element, element.hijo, url);
+                            }else{
+                                
+                            }
                         }
                     }
                     )}
@@ -58,7 +64,7 @@ const SidebarMenu = (props, list) => {
     const classes = props.classes;
     const theme = props.theme;
     let { path, url } = useRouteMatch();
-    if (url==='/') url='';
+    if (url === '/') url = '';
 
     const super_list = [];
     let new_list = [];
@@ -77,13 +83,13 @@ const SidebarMenu = (props, list) => {
         <Fragment>
             <Hidden smDown>
                 <Drawer className={classes.drawer} variant="persistent" open={open} classes={{ paper: classes.drawerPaper, }}>
-                    {sideList(classes, handleDrawer, theme, super_list,path,url)}
+                    {sideList(classes, handleDrawer, theme, super_list, path, url)}
                 </Drawer>
             </Hidden>
 
             <Hidden mdUp>
                 <Drawer variant="temporary" open={open} classes={{ paper: classes.drawerPaper, }} onClose={toggleDrawer(false)}>
-                    {sideList(classes, handleDrawer, theme, super_list,path,url)}
+                    {sideList(classes, handleDrawer, theme, super_list, path, url)}
                 </Drawer>
             </Hidden>
         </Fragment>
