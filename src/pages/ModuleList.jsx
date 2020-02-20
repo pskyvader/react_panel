e2fe5b636f911f5870ea43b1490c7f691d1288e6
Pablo <pskyvader@hotmail.com>
2020-02-20 13:56:59 -0600

    --- Auto-Git Commit ---

--- src/pages/ModuleList.jsx
+++ src/pages/ModuleList.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import ModuleCard from '../components/ModuleCard';
 import {
-    Typography,
+    Typography,Grid
 } from '@material-ui/core';
 import {
     useParams
@@ -10,14 +10,23 @@ import {
 export default () => {
     let { module,tipo } = useParams();
     console.log(useParams());
-    let array=new Array(10);
+    let array=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
     return (
-<div>
+        <Fragment>
     <Typography variant="h3">Module {module} {tipo? `tipo ${tipo}`:'' } </Typography>
-    {array.map(element => {
-        return <ModuleCard/>
-    })}
-        
-    </div>
+    <Grid
+  container
+  direction="row"
+  justify="flex-start"
+  alignItems="flex-start" spacing={3}
+>
+    {array.map((element,index) => {
+        return (
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <ModuleCard key={index}/>
+                </Grid>
+        )    })}
+        </Grid>
+        </Fragment>
     )
 };
