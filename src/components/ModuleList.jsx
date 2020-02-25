be5ecee546525a5718f737dcc1db341177296d76
Pablo <pskyvader@hotmail.com>
2020-02-25 17:43:37 -0600

    --- Auto-Git Commit ---

--- src/components/ModuleList.jsx
+++ src/components/ModuleList.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ModuleCard from '../components/ModuleCard';
 import { Grid } from '@material-ui/core';
-import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import Resolve from './Resolve';
 
 
 
@@ -19,15 +19,6 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-const GET_MODULE_LIST = gql`
-    query get_module_list ($idadministrador:Int!,$module:String!,$tipo:Int){
-        module(idadministrador:$idadministrador,module:$module){
-            titulo
-            estado
-        }
-    }`;
-
-
 
 function ModuleList(props) {
     const { module, tipo, config } = props;
@@ -37,15 +28,40 @@ function ModuleList(props) {
     } else if (!config) {
         return "Module " + module + " not allowed for this user";
     }
+    const module_data = config.hijo[0];
+    const fields_filter = module_data.permisos.mostrar.filter(x => (x['tipo'] === 'active' || x['tipo'] === 'text'));
+    const fields = fields_filter.map(x => x['field']);
 
-    const module_data=config.hijo[0];
-
-    const fields=module_data.permisos.mostrar.filter(x=>(x['tipo']==='active' || x['tipo']==='text'));
 
 
-    console.log(fields);
+    const vars = { first: 10, after: '' }
+    if (tipo>0){
+        vars['tipo']=tipo;
+    }
 
-    let array = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const table_query = 'all' + module;
+    const GET_LIST = gql(`
+    query get_list($first:Int!,$after:String,$tipo:Int){
+        $table(first:$first,after:$after,tipo:$tipo){
+            pageInfo{
+                endCursor
+                hasNextPage
+            }
+            edges{
+                node{
+                    $fields
+                }
+            }
+        }
+    }
+    `.replace('$table', table_query).replace('$fields', fields));
+
+    const { items, loading, loadMore, hasNextPage, error } = Resolve({ query: GET_LIST, table: table_query, vars: vars });
+
+    if (error){
+        return error;
+    }
+
     return (
         <Grid
             container
@@ -53,7 +69,7 @@ function ModuleList(props) {
             justify="flex-start"
             alignItems="flex-start" spacing={3}
         >
-            {array.map((element, index) => {
+            {items.map((element, index) => {
                 return (
                     <Grid item xs={12} sm className={classes.grid} key={index}>
                         <ModuleCard />
