dddcdf47f337377f32c885bc4697f6417ae4e3d4
Pablo <pskyvader@hotmail.com>
2020-02-27 17:39:10 -0600

    --- Auto-Git Commit ---

--- src/components/InfiniteList.jsx
+++ src/components/InfiniteList.jsx
@@ -1,25 +1,120 @@
-import React from 'react';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Immutable from 'immutable';
 import InfiniteLoader from "react-window-infinite-loader";
-import { makeStyles } from '@material-ui/core/styles';
-import VirtualizedList from './VirtualizedList';
 import { AutoSizer, List  } from 'react-virtualized';
+
 
 const STATUS_LOADING = 1;
 const STATUS_LOADED = 2;
 
-const useStyles = makeStyles(theme => ({
-    grid: {
-        display: 'flex',
-        justifyContent: 'center',
-        [theme.breakpoints.up('md')]: {
-            justifyContent: 'flex-start',
-        },
+export default class InfiniteLoaderExample extends React.PureComponent {
+  static contextTypes = {
+    list: PropTypes.instanceOf(Immutable.List).isRequired,
+  };
 
-    },
-}));
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loadedRowCount: 0,
+      loadedRowsMap: {},
+      loadingRowCount: 0,
+    };
 
-const rowRenderer=({index, key, style}) =>{
+    this._timeoutIdMap = {};
+
+    this._clearData = this._clearData.bind(this);
+    this._isRowLoaded = this._isRowLoaded.bind(this);
+    this._loadMoreRows = this._loadMoreRows.bind(this);
+    this._rowRenderer = this._rowRenderer.bind(this);
+  }
+
+  componentWillUnmount() {
+    Object.keys(this._timeoutIdMap).forEach(timeoutId => {
+      clearTimeout(timeoutId);
+    });
+  }
+
+  render() {
+    const {list} = this.context;
+
+    return (
+        <InfiniteLoader
+        isRowLoaded={this._isRowLoaded}
+        loadMoreRows={this._loadMoreRows}
+        rowCount={list.size}>
+        {({onRowsRendered, registerChild}) => (
+          <AutoSizer disableHeight>
+            {({width}) => (
+              <List
+                ref={registerChild}
+                height={200}
+                onRowsRendered={onRowsRendered}
+                rowCount={list.size}
+                rowHeight={30}
+                rowRenderer={this._rowRenderer}
+                width={width}
+              />
+            )}
+          </AutoSizer>
+        )}
+      </InfiniteLoader>
+    );
+  }
+
+  _clearData() {
+    this.setState({
+      loadedRowCount: 0,
+      loadedRowsMap: {},
+      loadingRowCount: 0,
+    });
+  }
+
+  _isRowLoaded({index}) {
+    const {loadedRowsMap} = this.state;
+    return !!loadedRowsMap[index]; // STATUS_LOADING or STATUS_LOADED
+  }
+
+  _loadMoreRows({startIndex, stopIndex}) {
+    const {loadedRowsMap, loadingRowCount} = this.state;
+    const increment = stopIndex - startIndex + 1;
+
+    for (var i = startIndex; i <= stopIndex; i++) {
+      loadedRowsMap[i] = STATUS_LOADING;
+    }
+
+    this.setState({
+      loadingRowCount: loadingRowCount + increment,
+    });
+
+    const timeoutId = setTimeout(() => {
+      const {loadedRowCount, loadingRowCount} = this.state;
+
+      delete this._timeoutIdMap[timeoutId];
+
+      for (var i = startIndex; i <= stopIndex; i++) {
+        loadedRowsMap[i] = STATUS_LOADED;
+      }
+
+      this.setState({
+        loadingRowCount: loadingRowCount - increment,
+        loadedRowCount: loadedRowCount + increment,
+      });
+
+      promiseResolver();
+    }, 1000 + Math.round(Math.random() * 2000));
+
+    this._timeoutIdMap[timeoutId] = true;
+
+    let promiseResolver;
+
+    return new Promise(resolve => {
+      promiseResolver = resolve;
+    });
+  }
+
+  _rowRenderer({index, key, style}) {
     const {list} = this.context;
     const {loadedRowsMap} = this.state;
 
@@ -40,69 +135,4 @@ const rowRenderer=({index, key, style}) =>{
       </div>
     );
   }
-
-const InfiniteList = ({ items, moreItemsLoading, loadMore, hasNextPage, columns, height }) => {
-    const isItemLoaded = index => !hasNextPage || index < items.length;
-    const itemCount = hasNextPage ? items.length + 1 : items.length;
-    const classes = useStyles();
-
-    const onScroll = ({ clientHeight, scrollHeight, scrollTop }) => {
-        if (scrollTop >= (scrollHeight - clientHeight) * 0.7) {
-            if (!moreItemsLoading && hasNextPage) {
-                loadMore();
-            }
-        }
-    };
-
-    const max_height = () => {
-        if (items.length * 48 <= height) {
-            return (items.length) * 48;
-        } else return height;
-    }
-
-    return (
-              <InfiniteLoader
-            isItemLoaded={isItemLoaded}
-            itemCount={itemCount}
-            loadMoreItems={loadMore}
-        >
-          {({onRowsRendered, registerChild}) => (
-            <AutoSizer disableHeight>
-              {({width}) => (
-                <List
-                  ref={registerChild}
-                  height={200}
-                  onRowsRendered={onRowsRendered}
-                  rowCount={items.length}
-                  rowHeight={30}
-                  rowRenderer={rowRenderer}
-                  width={width}
-                />
-              )}
-            </AutoSizer>
-          )}
-        </InfiniteLoader>
-    )
-
-    return (
-        <InfiniteLoader
-            isItemLoaded={isItemLoaded}
-            itemCount={itemCount}
-            loadMoreItems={loadMore}
-        >
-            {({ onRowsRendered, registerChild }) => (
-                <VirtualizedList
-                    rowCount={items.length}
-                    rowGetter={({ index }) => items[index]}
-                    columns={columns}
-                    ref={registerChild}
-                    onRowsRendered={onRowsRendered}
-                    onScroll={onScroll}
-                    loading={moreItemsLoading}
-                />
-            )}
-        </InfiniteLoader>
-
-    )
-};
-export default InfiniteList;
+}
