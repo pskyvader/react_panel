6939dbb49f88f248053ea34f9dd9fb28a3e22a48
Pablo <pskyvader@hotmail.com>
2020-02-28 16:22:04 -0600

    --- Auto-Git Commit ---

--- src/components/ModuleList.jsx
+++ src/components/ModuleList.jsx
@@ -9,7 +9,16 @@ function ModuleList(props) {
     if (config !== null && config !== false) {
         const module_data = config.hijo[0];
         const fields_filter = module_data.permisos.mostrar.filter(x => (x['tipo'] === 'active' || x['tipo'] === 'text'));
-        fields = fields_filter.map(x => x['field']);
+        fields = fields_filter.map(x => {
+            let field=x['field'].split('_');
+            field.map((y,i)=>{
+                console.log(y,i);
+                return y;
+            });
+            let field2=field.map((y,i)=> (i===0)? y.charAt(0).toUpperCase() + y.slice(1):y);
+            let field3=field2.join("");
+            return field3; 
+        });
     }
 
     const vars = { first: 10, after: '' }
