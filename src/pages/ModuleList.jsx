b0f4c65b7ef68f864fa8b4ac0dba616034d024c0
Pablo <pskyvader@hotmail.com>
2020-03-03 20:54:06 -0600

    --- Auto-Git Commit ---

--- src/pages/ModuleList.jsx
+++ src/pages/ModuleList.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import ModuleConfiguration from '../components/ModuleConfiguration';
 import { Typography, Container } from '@material-ui/core';
 import { useParams } from "react-router-dom";
-import { useRef } from 'react'
-
 
 export default () => {
-    const inputRef = useRef();
     let { module, tipo } = useParams();
     if (typeof (tipo) == 'undefined') {
         tipo = 0;
     }
-    console.log(inputRef);
     return (
         <Container maxWidth="xl" fixed>
-            <Typography ref={inputRef}  variant="h3" gutterBottom >Module {module} {tipo ? `tipo ${tipo}` : ''} </Typography>
-            <ModuleConfiguration module={module} tipo={tipo} idadministrador="1" offsetTop={inputRef.current} />
+            <Typography variant="h3" gutterBottom >Module {module} {tipo ? `tipo ${tipo}` : ''} </Typography>
+            <ModuleConfiguration module={module} tipo={tipo} idadministrador="1" />
         </Container>
     )
 };
