5d88a1a8fd9ce0a6e7aebea292f87c3c7a658d1d
Pablo <pskyvader@hotmail.com>
2020-03-11 16:29:38 -0500

    --- Auto-Git Commit ---

--- src/components/module/ModuleList.jsx
+++ src/components/module/ModuleList.jsx
@@ -3,18 +3,22 @@ import { gql } from 'apollo-boost';
 import Resolve from '../Resolve';
 import InfiniteList from './InfiniteList';
 
+
+
+const formatField=(field)=>{
+    field=field.split('_');
+    let field2=field.map((y,i)=> (i>0)? y.charAt(0).toUpperCase() + y.slice(1):y);
+    let field3=field2.join("");
+    return field3; 
+}
+
 function ModuleList(props) {
     let { module, tipo, config } = props;
     let fields = ['id'];
     if (config !== null && config !== false) {
         const module_data = config.hijo[0];
-        const fields_filter = module_data.permisos.mostrar.filter(x => (x['tipo'] === 'active' || x['tipo'] === 'text'));
-        fields = fields_filter.map(x => {
-            let field=x['field'].split('_');
-            let field2=field.map((y,i)=> (i>0)? y.charAt(0).toUpperCase() + y.slice(1):y);
-            let field3=field2.join("");
-            return field3; 
-        });
+        const fields_filter = module_data.permisos.mostrar.filter(x => (x['tipo'] !== 'action' && x['tipo'] !== 'delete'));
+        fields = fields_filter.map(x => formatField(x['field']));
     }
 
     const vars = { first: 100, after: ''}
